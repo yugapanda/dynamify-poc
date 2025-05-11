59a41c96341426fd8b62e004a22ce81e1d5d154d
yugapanda <xpamda@gmail.com>
2025-05-12 00:09:01 +0900

    サーバーのバインドアドレスを127.0.0.1から0.0.0.0に変更し、外部からのアクセスを可能にしました。

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ async fn main() -> std::io::Result<()> {
             .service(generate_text)
             .service(files::Files::new("/static", "static").show_files_listing())
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
